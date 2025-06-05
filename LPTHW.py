770c2996aeb1f4578553ec3ec486a214f89cfa64
TangentJay <78071392+TangentJay@users.noreply.github.com>
2025-06-05 07:41:01 -0500

    Create LPTHW.py

---
+++ LPTHW.py
@@ -0,0 +1,20 @@
+my_name = 'bill'
+my_age = 1337
+my_height = 72 #inches
+my_weight = 155
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'black'
+
+#fstrings  or fotmat strings
+
+print(f'Let\s talk about {my_age}.')
+print(f'He"s {my_height} inches tall')
+print(f'He"s {my_weight} es pounds light ')
+print('Pretty light!')
+print(f'He"s got {my_eyes} eyes and {my_hair} hair. ')
+print(f'His teeth are usually {my_teeth} depending on  the care.')
+
+#this line tricky?
+total=my_age + my_height + my_weight
+print(f'If I add {my_age}, {my_height}, und {my_weight} I get {total} .')
