fd9198cae03bbd7716be4674301d9a52ec3057ed
TangentJay <78071392+TangentJay@users.noreply.github.com>
2025-06-07 00:46:36 -0500

    Create essenR.py

---
+++ essenR.py
@@ -0,0 +1,15 @@
+# Write code below 💖
+rating=float(input("Enter you review for teh food! "))
+
+if rating > 4.5:
+  print("Extraordinary")
+elif rating >4:
+  print('Excellent')
+elif rating > 3:
+  print("good")
+elif rating >2:
+  print("fair")
+else:
+  print('Poor')
+
+
