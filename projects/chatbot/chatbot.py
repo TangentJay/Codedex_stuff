abf5e149dd1519d1a20e7fe5e1ca0768ddd9150e
TangentJay <78071392+TangentJay@users.noreply.github.com>
2025-09-07 21:25:55 -0500

    Update chatbot.py

--- projects/chatbot/chatbot.py
+++ projects/chatbot/chatbot.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key='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')
+client = OpenAI()
 
 prompt = input('prompt: ')
 
