8a75b3ea030db4cd277a964702890762188fbc2e
TangentJay <78071392+TangentJay@users.noreply.github.com>
2025-09-17 22:54:14 -0500

    s

---
+++ CODedex/cplus/bunny.cpp
@@ -0,0 +1,17 @@
+// Write code below 💖
+#include <iostream>
+
+int main() {
+std::cout << "9,17,2025";
+std::cout <<" ______________";
+std::cout <<"|     Ello  24/7|";
+std::cout <<"|    Guys   GRIND|";
+std::cout <<"|    -N-        |";
+std::cout <<"|    Gals       |";
+std::cout <<"|_______________| ";
+std::cout <<"(|______/) || ";
+std::cout <<"(⚪👃🏽🟢 ) || ";
+std::cout <<"/        づ";
+
+
+}
